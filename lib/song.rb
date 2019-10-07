8ba9c64a05f2d066c6187232da30397604b1adaa
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-07 23:59:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,11 @@ class Song
   attr_accessor :name, :artist, :genre 
   @@all = []
   
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+  
   def self.all 
     @all 
   end
